4930a85537323298c184ca81d7f0a4710d86c8ba
angelperez0709 <angelperez07092000@gmai.com>
2023-12-12 09:52:33 +0100

    add tests and create_appointment function

--- src/test/java/com/example/demo/EntityControllerUnitTest.java
+++ src/test/java/com/example/demo/EntityControllerUnitTest.java
@@ -33,69 +33,158 @@ import com.example.demo.repositories.*;
 import com.example.demo.entities.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
-
-/** TODO
+/**
+ * TODO
  * Implement all the unit test in its corresponding class.
  * Make sure to be as exhaustive as possible. Coverage is checked ;)
  */
 
 @WebMvcTest(DoctorController.class)
-class DoctorControllerUnitTest{
+class DoctorControllerUnitTest {
 
     @MockBean
     private DoctorRepository doctorRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    public void shouldCreateDoctor() throws Exception {
+        Doctor doctor = new Doctor("John", "Brown", 28, "doctor@example.com");
+        mockMvc.perform(post("/api/doctor").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(doctor)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldDeleteDoctorById() throws Exception {
+        Doctor doctor = new Doctor("John", "Brown", 28, "doctor@example.com");
+        mockMvc.perform(delete("/api/doctor/" + doctor.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldNotDeletePatientById() throws Exception {
+        long id = 21;
+        mockMvc.perform(delete("/api/doctor/" + id))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shoudlGetDoctorById() throws Exception {
+        Doctor doctor = new Doctor("John", "Brown", 28, "doctor@example.com");
+        mockMvc.perform(get("/api/doctor/" + doctor.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shoudlNotGetDoctorById() throws Exception {
+        long id = 0;
+        mockMvc.perform(get("/api/doctor/" + id))
+                .andExpect(status().isNotFound());
     }
 }
 
-
 @WebMvcTest(PatientController.class)
-class PatientControllerUnitTest{
+class PatientControllerUnitTest {
 
     @MockBean
     private PatientRepository patientRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    public void shouldCreatePatient() throws Exception {
+        Patient patient = new Patient("John", "Brown", 28, "patient@example.com");
+        mockMvc.perform(post("/api/patient").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(patient)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldDeletePatientById() throws Exception {
+        Patient patient = new Patient("Alfonso", "Tello", 28, "patient@example.com");
+        mockMvc.perform(delete("/api/patient/" + patient.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldNotDeletePatientById() throws Exception {
+        long id = 21;
+        mockMvc.perform(delete("/api/patient/" + id))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shoudlGetPatientById() throws Exception {
+        Patient patient = new Patient("Alfonso", "Tello", 28, "patient@example.com");
+        mockMvc.perform(get("/api/patient/" + patient.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shoudlNotGetPatientById() throws Exception {
+        long id = 0;
+        mockMvc.perform(get("/api/patient/" + id))
+                .andExpect(status().isNotFound());
     }
 
 }
 
 @WebMvcTest(RoomController.class)
-class RoomControllerUnitTest{
+class RoomControllerUnitTest {
 
     @MockBean
     private RoomRepository roomRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    public void shouldCreateRoom() throws Exception {
+        Room room = new Room("room");
+        mockMvc.perform(post("/api/room").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(room)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldDeleteRoomByRoomName() throws Exception {
+        Room room = new Room("room");
+        mockMvc.perform(delete("/api/doctor").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(room.getRoomName())))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shoudlNotDeleteRoomByRoomName() throws Exception {
+        String roomName = "NotRoomName";
+        mockMvc.perform(delete("/api/patient/" + roomName))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shoudlGetRoomByRoomName() throws Exception {
+        Room room = new Room("room");
+        mockMvc.perform(get("/api/patient/" + room.getRoomName()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shoudlNotGetRoomByRoomName() throws Exception {
+        String roomName = "NotRoomName";
+        mockMvc.perform(get("/api/patient/" + roomName))
+                .andExpect(status().isNotFound());
     }
 
 }
